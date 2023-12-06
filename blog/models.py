342e1851406478f2770e56a1a617cfbcac603d0a
Alma Broman <alma.broman@gmail.com>
2023-12-06 13:48:20 +0000

    Build and configure the django admin panel

--- blog/models.py
+++ blog/models.py
@@ -9,30 +9,30 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     updated_on = models.DateTimeField(auto_now=True)
     description = models.TextField()
-    featured_image = CloudinaryField('image', default='placeholder')
-    created_on = models.DateTimeField(auto_now_add=True)
-    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
     event_start = models.DateTimeField()
     event_end = models.DateTimeField()
     adress = models.CharField('adress', max_length=1024)
-    google_maps_link = models.URLField(max_length=200)
-    additional_link = models.URLField(max_length=200)
+    map_link = models.URLField(max_length=200)
+    featured_image = CloudinaryField('image', default='placeholder')
+    created_on = models.DateTimeField(auto_now_add=True)
     category = models.CharField(max_length=200)
-    approved = models.BooleanField(default=False)
+    additional_link = models.URLField(max_length=200, blank=True)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
 
     class Meta:
         ordering = ['-event_start']
 
-    def __str__(Self):
+    def __str__(self):
         return self.title
     
     def number_of_likes(self):
         return self.likes.count()
 
-class Comment(models.Model):
 
+class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=80)
+    email = models.EmailField()
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
