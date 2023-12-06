c0061aaeb256ca534205acdd58c80b06191856a8
Alma Broman <alma.broman@gmail.com>
2023-12-06 12:21:08 +0000

    Create models for Posts and Comments

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    updated_on = models.DateTimeField(auto_now=True)
+    description = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    created_on = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+    event_start = models.DateTimeField()
+    event_end = models.DateTimeField()
+    adress = models.CharField('adress', max_length=1024)
+    google_maps_link = models.URLField(max_length=200)
+    additional_link = models.URLField(max_length=200)
+    category = models.CharField(max_length=200)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-event_start']
+
+    def __str__(Self):
+        return self.title
+    
+    def number_of_likes(self):
+        return self.likes.count()
+
+class Comment(models.Model):
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+    
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
